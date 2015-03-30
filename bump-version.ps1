03ef5b91724ea6ebb4a63e716a2200bb16c68bbf
nagits <Aleksey.nagits@gmail.com>
2015-03-31 03:05:22 +0600

    Add bump-version script.

---
+++ bump-version.ps1
@@ -0,0 +1,32 @@
+﻿$packageFile = "Source\Core\BuildVisionPackage.Package.cs"
+$manifestFile = "Source\source.extension.vsixmanifest"
+$appveyorFile = "appveyor.yml"
+
+If ($env:APPVEYOR)
+{
+    $version = $env:APPVEYOR_BUILD_VERSION
+}
+Else
+{
+    $msg = 'Enter a new version in 0.0.0 format (or enter nothing to exit)'
+    $version = (Read-Host $msg).Trim()
+    if(!$version)
+    { 
+        echo "Aborted."
+        Exit 
+    }
+
+    (Get-Content $appveyorFile) `
+        -replace '(version: )[0-9.]*(\.{build})', "`${1}$version`$2" |
+      Out-File $appveyorFile
+}
+
+(Get-Content $packageFile) `
+    -replace '(PackageVersion) = "[0-9.]*"', "`${1} = ""$version""" |
+  Out-File $packageFile
+
+(Get-Content $manifestFile) `
+    -replace '(<Version>)[0-9.]*', "`${1}$version" |
+  Out-File $manifestFile
+
+echo "Done."

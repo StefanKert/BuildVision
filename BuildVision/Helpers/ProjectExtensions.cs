85899150ecc0fa1e8157a4a1b3dc13e5b04659de
Stefan Kert <stefan.kert@sbg.at>
2017-04-05 22:21:31 +0200

    Moved extension methods for specific types to extra classes

--- BuildVision/Helpers/ProjectExtensions.cs
+++ BuildVision/Helpers/ProjectExtensions.cs
@@ -194,7 +194,7 @@ namespace AlekseyNagovitsyn.BuildVision.Helpers
         /// <returns>The found file or <c>null</c>.</returns>
         private static ProjectItem FindProjectItem(this Project project, string filePath)
         {
-            return FindProjectItem(project.ProjectItems, filePath);
+            return project.ProjectItems.FindProjectItem(filePath);
         }
 
         public static string GetFrameworkString(this Project project)
@@ -499,59 +499,6 @@ namespace AlekseyNagovitsyn.BuildVision.Helpers
             }
         }
 
-        /// <summary>
-        /// Find file in projects collection.
-        /// </summary>
-        /// <param name="items">Projects collection.</param>
-        /// <param name="filePath">File path, relative to the <paramref name="items"/> root.</param>
-        /// <returns>The found file or <c>null</c>.</returns>
-        private static ProjectItem FindProjectItem(this ProjectItems items, string filePath)
-        {
-            if (string.IsNullOrEmpty(filePath))
-                throw new ArgumentException("Argument `filePath` is null or empty.", "filePath");
-
-            int backslashIndex = filePath.IndexOf("\\", StringComparison.Ordinal);
-            bool findFolder = (backslashIndex != -1);
-            if (findFolder)
-            {
-                string folderName = filePath.Substring(0, backslashIndex);
-                foreach (ProjectItem item in items)
-                {
-                    if (item.Kind != EnvDTE.Constants.vsProjectItemKindVirtualFolder &&
-                        item.Kind != EnvDTE.Constants.vsProjectItemKindPhysicalFolder)
-                        continue;
-
-                    if (folderName == item.Name)
-                    {
-                        string nextpath = filePath.Substring(backslashIndex + 1);
-                        return FindProjectItem(item.ProjectItems, nextpath);
-                    }
-                }
-            }
-            else
-            {
-                string fileName = filePath;
-                foreach (ProjectItem item in items)
-                {
-                    if (item.Kind != EnvDTE.Constants.vsProjectItemKindPhysicalFile)
-                        continue;
-
-                    if (item.Name == fileName)
-                        return item;
-
-                    // Nested item, e.g. Default.aspx or MainWindow.xaml.
-                    if (item.ProjectItems.Count > 0)
-                    {
-                        ProjectItem childItem = FindProjectItem(item.ProjectItems, fileName);
-                        if (childItem != null)
-                            return childItem;
-                    }
-                }
-            }
-
-            return null;
-        }
-
         public static string GetRootNamespace(this Project project)
         {
             var val = project.TryGetPropertyValueOrDefault("RootNamespace") ?? project.TryGetPropertyValueOrDefault("DefaultNamespace");
@@ -566,18 +513,7 @@ namespace AlekseyNagovitsyn.BuildVision.Helpers
             if (project.IsDirty)
                 return true;
 
-            if (project.ProjectItems != null && project.ProjectItems.Cast<ProjectItem>().Any(ProjectItemIsDirty))
-                return true;
-
-            return false;
-        }
-
-        private static bool ProjectItemIsDirty(this ProjectItem projectItem)
-        {
-            if (projectItem.IsDirty)
-                return true;
-
-            if (projectItem.ProjectItems != null && projectItem.ProjectItems.Cast<ProjectItem>().Any(ProjectItemIsDirty))
+            if (project.ProjectItems != null && project.ProjectItems.Cast<ProjectItem>().Any(x => x.ProjectItemIsDirty()))
                 return true;
 
             return false;
@@ -588,7 +524,7 @@ namespace AlekseyNagovitsyn.BuildVision.Helpers
         /// </summary>
         /// <param name="project">The project - owner of the Error Item.</param>
         /// <param name="errorItem">The Error Item.</param>
-        public static bool NavigateToErrorItem(this Project project, Tool.Building.ErrorItem errorItem)
+        public static bool NavigateToErrorItem(this Project project, ErrorItem errorItem)
         {
             try
             {
@@ -648,6 +584,7 @@ namespace AlekseyNagovitsyn.BuildVision.Helpers
 
             return true;
         }
+
         public static string GetTreePath(this Project project, bool includeSelfProjectName = true)
         {
             var path = new StringBuilder();

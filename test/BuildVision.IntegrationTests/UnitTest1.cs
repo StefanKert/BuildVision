3bbafd406f4f443cfc9d218210b1bb0e7a9708b5
Stefan Kert <stefan.kert@fiskaltrust.at>
2018-09-02 18:08:43 +0200

    Seperate between src and test

---
+++ test/BuildVision.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildVision.IntegrationTests
+{
+    [TestClass]
+    public class PackageTests
+    {
+        //[TestMethod]
+        //[HostType("VS IDE")]
+        //public void PackageLoadTest()
+        //{
+        //    UIThreadInvoker.Invoke(new Action(() =>
+        //    {
+        //        // Get the Shell Service
+        //        var shellService = VsIdeTestHostContext.ServiceProvider.GetService(typeof(SVsShell)) as IVsShell;
+        //        Assert.IsNotNull(shellService);
+
+        //        // Validate package load
+        //        IVsPackage package;
+        //        var packageGuid = new Guid(PackageGuids.GuidCodeMaidPackageString);
+
+        //        Assert.IsTrue(0 == shellService.LoadPackage(ref packageGuid, out package));
+        //        Assert.IsNotNull(package, "Package failed to load");
+        //    }));
+        //}
+    }
+}

334f6c2843d927e31dc39baa1d9f3040597f7cc4
Stefan Kert <stefan.kert@sbg.at>
2019-03-21 06:58:42 +0100

    Added xunit 2.4.1 and description for tests

--- test/BuildVision.IntegrationTests/PackageTests.cs
+++ test/BuildVision.IntegrationTests/PackageTests.cs
@@ -18,7 +18,7 @@ namespace BuildVision.IntegrationTests
         private static IVsUIShell UiShellService => GlobalServices.GetService<SVsUIShell>() as IVsUIShell;
         private static DTE DTE => GlobalServices.GetService<DTE>() as DTE;
 
-        [VsixFact("15.0")]
+        [VsixFact("15.0", DisplayName = "PackageLoad_Should_Succeed")]
         public void PackageLoad_Should_Succeed()
         {
             IVsPackage package;
@@ -29,7 +29,7 @@ namespace BuildVision.IntegrationTests
             Assert.NotNull(package);
         }
 
-        [VsixFact("15.0")]
+        [VsixFact("15.0", DisplayName = "ClickOnBuildVisionMenuItem_Should_ShowBuildVision")]
         public void ClickOnBuildVisionMenuItem_Should_ShowBuildVision()
         {
             var toolwndCommandId = new CommandID(PackageGuids.GuidBuildVisionCmdSet, (int) PackageIds.CmdIdBuildVisionToolWindow);
